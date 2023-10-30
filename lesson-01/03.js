7240e8429b024361464e076982248c9a9c9129cb
Halanau327 <a.halanau@gmail.com>
2023-10-30 18:53:47 +0300

    add 03.js file

---
+++ lesson-01/03.js
@@ -0,0 +1,6 @@
+​let number = 1;
+
+console.log(number); 
+console.log(++number + --number); 
+console.log(++number + --number); 
+console.log(number);
--- lessons zoom/index.js
+++ lessons zoom/index.js
@@ -5,3 +5,15 @@ if (x == y) {
 } else if (x === y) {
     console.log("х и у равны по значению, но не равны по типу")
 }
+
+
+let count1 = 10;
+let count2 = 20;
+let total = 0;
+
+count1 += 3;
+count2 -= 5;
+total = count1 * count2; 
+console.log(total);
+
+let m
