9f986d40976799b6e86b3c29fc6c7be6615ce98b
Halanau327 <a.halanau@gmail.com>
2023-10-31 00:11:15 +0300

    update 05.js

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -6,6 +6,7 @@ switch (true) {
     case rule.length > 25:
     console.log("У меня 100 пудов все получится");
     break;
-    default: console.log("50 на 50");
+    case rule.length = 25:
+    console.log("50 на 50");
     break;
 }
