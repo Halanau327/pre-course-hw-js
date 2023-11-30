df0ee31b2b8f68b46a0e03d8e6197f50b5987340
Halanau327 <a.halanau@gmail.com>
2023-11-30 23:39:37 +0300

    add 01.js lesson 3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,9 +1,8 @@
 ​const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    const filterColors = colors.filter (color => color === "черный" || color === "красный" || color === "желтый");
-    const colorsString = filterColors.join("-");
-    console.log(colorsString);
-}
+    return colors.filter (color => color === "черный" || color === "красный" || color === "желтый").join("-");
+    
+};
 
-createColorString();
+console.log(createColorString());
