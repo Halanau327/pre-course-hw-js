3890af08782e75191120753cc38784789fe9d3da
Halanau327 <a.halanau@gmail.com>
2023-12-01 21:08:33 +0300

    add

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,8 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
-function createColorString() {
-    return colors.filter (colors => colors === "черный" || color === "красный" || color === "желтый").join('-');
-    
+function createColorString () {
+    return colors.filter(color => color === "черный" || color === "красный" || color === "желтый").join('-');
 };
 
 console.log(createColorString());
