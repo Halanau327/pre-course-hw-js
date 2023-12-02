fa0e04417e2b8ae8430f295b37e035bffc9ccd1f
Halanau327 <a.halanau@gmail.com>
2023-12-02 14:14:24 +0300

    update 03.js

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,4 @@
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
-const result = fruits.slice(1,8);
-console.log(result);
+const results = fruits.slice(1,8);
+
+console.log(results);
