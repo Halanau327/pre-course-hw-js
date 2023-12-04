22a8b47f4d871eb197dc58dde001a376e42d4d9d
Halanau327 <a.halanau@gmail.com>
2023-12-04 20:50:45 +0300

    add 07.js

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -5,5 +5,5 @@ const numbers = [
 ];
 
 
-const result = numbers.map((row) => row.filter(num => num % 2 === 0));
-console.log(result);
+const map = numbers.map((row) => row.filter(num => num % 2 === 0));
+console.log(map);
