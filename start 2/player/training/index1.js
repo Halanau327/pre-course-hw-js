19d43862e5795e53e917062eda60fed02a273b51
Halanau327 <a.halanau@gmail.com>
2024-01-24 16:45:19 +0300

    add training files

--- start 2/player/training/index1.js
+++ start 2/player/training/index1.js
@@ -1,33 +1,51 @@
-let footballTeams = {
-    teams: [
-      {
-        name: "FC Barcelona",
-        country: "Spain",
-        homeStadium: "Camp Nou",
-        coach: ["Ronald Koeman"],
-        players: ["Lionel Messi", "Gerard Piqué", "Jordi Alba", "Antoine Griezmann"]
-      },
-      {
-        name: "Real Madrid",
-        country: "Spain",
-        homeStadium: "Santiago Bernabeu",
-        coach: ["Carlo Ancelotti"],
-        players: ["Karim Benzema", "Sergio Ramos", "Toni Kroos", "Eden Hazard"]
-      },
-      {
-        name: "Manchester United",
-        country: "England",
-        homeStadium: "Old Trafford",
-        coach: ["Ole Gunnar Solskjær"],
-        players: ["Bruno Fernandes", "Harry Maguire", "Paul Pogba", "Marcus Rashford"]
-      }
-    ]
-  };
+const footballClubs = [
+  {
+    name: 'FC Barcelona',
+    country: 'Spain',
+    coach: 'Ronald Koeman',
+    captain: 'Lionel Messi',
+    stadium: {
+      name: 'Camp Nou',
+      capacity: 99354,
+      location: 'Barcelona, Spain'
+    },
+    players: [
+      { name: 'Lionel Messi', position: 'Forward', number: 10 },
+      { name: 'Gerard Piqué', position: 'Defender', number: 3 },
+      { name: 'Marc-André ter Stegen', position: 'Goalkeeper', number: 1 },
+    ],
+    achievements: ['UEFA Champions League', 'La Liga', 'Copa del Rey']
+  },
+  {
+    name: 'Real Madrid CF',
+    country: 'Spain',
+    coach: 'Carlo Ancelotti',
+    captain: 'Sergio Ramos',
+    stadium: {
+      name: 'Santiago Bernabéu',
+      capacity: 81044,
+      location: 'Madrid, Spain'
+    },
+    players: [
+      { name: 'Karim Benzema', position: 'Forward', number: 9 },
+      { name: 'Raphael Varane', position: 'Defender', number: 5 },
+      { name: 'Thibaut Courtois', position: 'Goalkeeper', number: 1 },
+    ],
+    achievements: ['UEFA Champions League', 'La Liga', 'Copa del Rey']
+  }
+];
 
-  let teamNames = [];
 
-  footballTeams.teams.forEach(team => {
-    teamNames.push(team.name);
-  });
-  
-  console.log(teamNames);
+
+function renderPlayerName(footballClubs) {
+  let clubs = '';
+for (let i = 0; i < footballClubs.length; i++) {
+  clubs = footballClubs[i].players;
+  clubs.forEach(player => {
+    console.log(player.name);
+  })
+}
+return clubs;
+}
+
+renderPlayerName(footballClubs);
